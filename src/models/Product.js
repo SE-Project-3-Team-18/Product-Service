4afc7836eb5041d1e91a378956d417eff58fc3ad
ROHIT32767 <rohitgowlapalli01@gmail.com>
2024-04-13 12:16:39 +0530

    Add new files and configurations for logging, entrypoint, middleware, config, index, Dockerfile, package.json, Product model, and app.js

---
+++ src/models/Product.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  seller :{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+},
+toJSON = {
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
